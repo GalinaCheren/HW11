9c7e7ffaa6c21833f0a41a0263da35300b0869dd
GalinaCheren <galinacheren8@gmail.com>
2022-10-21 20:12:31 +0300

    fixed switch

--- index.js
+++ index.js
@@ -15,18 +15,18 @@
 
 let numOrStr = prompt("input number or string");
 console.log(numOrStr);
-if (isNaN(+numOrStr)) {
-  console.log(" number is Ba_NaN");
-} else {
-  switch (numOrStr) {
-    case null:
-      console.log("ви скасували");
-      break;
-    case "":
-      console.log("Empty String");
-      break;
-    default:
-      console.log("OK!");
-      break;
-  }
+
+switch (true) {
+  case numOrStr === null:
+    console.log("ви скасували");
+    break;
+  case numOrStr.trim() === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log(" number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
+    break;
 }
